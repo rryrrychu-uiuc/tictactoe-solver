c22d80899ce6054b21e98645bc82f59c99c9afb2
rryrrychu-uiuc <harryc3@illinois.edu>
2021-03-08 18:51:49 -0500

    Added X winner and O winner test cases

--- tests/test-tictactoe.cc
+++ tests/test-tictactoe.cc
@@ -38,8 +38,12 @@ TEST_CASE("Boards with no winner") {
     REQUIRE(Board("xxoooxxxo").EvaluateBoard() == BoardState::NoWinner);
   }
 
-  SECTION("Partial board with no winner") {
+  SECTION("Partial X move board with no winner") {
     REQUIRE(Board("x........").EvaluateBoard() == BoardState::NoWinner);
+  }
+
+  SECTION("Partial O move board with no winner") {
+    REQUIRE(Board("x.....o...").EvaluateBoard() == BoardState::NoWinner);
   }
 
   SECTION("Empty board with no winner") {
@@ -69,11 +73,80 @@ TEST_CASE("Boards with UnreachableState") {
   }
 
   SECTION("Too many losing X marks") {
-    REQUIRE(Board("xoxxox.o.").EvaluateBoard() == BoardState::UnreachableState);
+    REQUIRE(Board("xoxxox.oo").EvaluateBoard() == BoardState::UnreachableState);
   }
 
   SECTION("Too many losing O marks") {
     REQUIRE(Board("xxxoo...o").EvaluateBoard() == BoardState::UnreachableState);
+  }
+}
+
+TEST_CASE("Boards with X Win") {
+
+  SECTION("Valid X Win") {
+    REQUIRE(Board("xxxoo....").EvaluateBoard() == BoardState::Xwins);
+  }
+
+  SECTION("Valid X Multiple Wins") {
+    REQUIRE(Board("xxxxooxoo").EvaluateBoard() == BoardState::Xwins);
+  }
+
+  SECTION("Edge Row X Win") {
+    REQUIRE(Board("...oo.xxx").EvaluateBoard() == BoardState::Xwins);
+  }
+
+  SECTION("Middle Row X Win") {
+    REQUIRE(Board("o..xxxo..").EvaluateBoard() == BoardState::Xwins);
+  }
+
+  SECTION("Edge Column X Win") {
+    REQUIRE(Board(".ox.ox..x").EvaluateBoard() == BoardState::Xwins);
+  }
+
+  SECTION("Middle Column X Win") {
+    REQUIRE(Board("ox..xo.x.").EvaluateBoard() == BoardState::Xwins);
+  }
+
+  SECTION("Left Diagonal X Win") {
+    REQUIRE(Board("x.o.x.o.x").EvaluateBoard() == BoardState::Xwins);
+  }
+
+  SECTION("Right Diagonal X Win") {
+    REQUIRE(Board("o.x.x.x.o").EvaluateBoard() == BoardState::Xwins);
+  }
+
+
+
+}
+
+TEST_CASE("Boards with O Win"){
+
+  SECTION("Valid O Win") {
+    REQUIRE(Board("oooxx.x..").EvaluateBoard() == BoardState::Owins);
+  }
+
+  SECTION("Edge Row O Win") {
+    REQUIRE(Board("...xx.ooo").EvaluateBoard() == BoardState::Owins);
+  }
+
+  SECTION("Middle Row O Win") {
+    REQUIRE(Board("x..oooxx.").EvaluateBoard() == BoardState::Owins);
+  }
+
+  SECTION("Edge Column O Win") {
+    REQUIRE(Board(".xoxxo..o").EvaluateBoard() == BoardState::Owins);
+  }
+
+  SECTION("Middle Column O Win") {
+    REQUIRE(Board("xo.xox.o.").EvaluateBoard() == BoardState::Owins);
+  }
+
+  SECTION("Left Diagonal O Win") {
+    REQUIRE(Board("o.x.oxx.o").EvaluateBoard() == BoardState::Owins);
+  }
+
+  SECTION("Right Diagonal O Win") {
+    REQUIRE(Board("x.o.o.oxx").EvaluateBoard() == BoardState::Owins);
   }
 }
 
